489632fd071e8c0b29cbb9952450150707191de1
Clemens Reichelt <creichelt@me.com>
2021-02-28 15:10:07 +0100

    adding project 20: created snake body and movement

---
+++ 020_21_snake/snake.py
@@ -0,0 +1,47 @@
+from turtle import Turtle
+STARTING_POS =[(0, 0), (-20, 0), (-40, 0)]
+MOVING_DIST = 20
+RIGHT = 0
+UP = 90
+LEFT = 180
+DOWN = 270
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create()
+        self.head = self.segments[0]
+
+    def create(self):
+        for pos in STARTING_POS:
+            """creating 3 starting segments"""
+            snake = Turtle('square')
+            snake.color('chartreuse4')
+            snake.penup()
+            snake.goto(pos)
+            self.segments.append(snake)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            x = self.segments[seg_num - 1].xcor()
+            y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(x, y)
+        self.head.fd(MOVING_DIST)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
